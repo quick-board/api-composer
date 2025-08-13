0efd2afcd21f4f77688c2a4f5143123141c37adb
seungchan <tmdcks3489@gmail.com>
2025-08-13 18:34:09 +0900

    🎉 Begin

---
+++ src/main/java/com/quickboard/apicomposer/ApiComposerApplication.java
@@ -0,0 +1,13 @@
+package com.quickboard.apicomposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComposerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickboard/apicomposer/ApiComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickboard.apicomposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComposerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
