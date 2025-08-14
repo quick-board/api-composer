813bc930b22539dab3ed58185e6400eca792fd79
seungchan <tmdcks3489@gmail.com>
2025-08-14 21:57:26 +0900

    ➖ Remove webflux

--- src/main/java/com/quickboard/apicomposer/common/config/WebApiConfig.java
+++ src/main/java/com/quickboard/apicomposer/common/config/WebApiConfig.java
@@ -1,49 +1,12 @@
 package com.quickboard.apicomposer.common.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebApiConfig {
-
-    @Value("${services.post.url}")
-    public String postUrl;
-    @Value("${services.board.url}")
-    public String boardUrl;
-    @Value("${services.comment.url}")
-    public String commentUrl;
-    @Value("${services.profile.url}")
-    public String profileUrl;
-
-    @Bean
-    @LoadBalanced
-    public WebClient.Builder webClientBuilder(){
-        return WebClient.builder();
-    }
-
-    @Bean("postWebClient")
-    public WebClient postWebClient(WebClient.Builder webClientBuilder){
-        return webClientBuilder.baseUrl(postUrl).build();
-    }
-
-    @Bean("boardWebClient")
-    public WebClient boardWebClient(WebClient.Builder webClientBuilder){
-        return webClientBuilder.baseUrl(boardUrl).build();
-    }
-
-    @Bean("commentWebClient")
-    public WebClient commentWebClient(WebClient.Builder webClientBuilder){
-        return webClientBuilder.baseUrl(commentUrl).build();
-    }
-
-    @Bean("profileWebClient")
-    public WebClient profileWebClient(WebClient.Builder webClientBuilder){
-        return webClientBuilder.baseUrl(profileUrl).build();
-    }
 
     @Bean
     @LoadBalanced
--- src/main/java/com/quickboard/apicomposer/post/controller/TestController.java
+++ src/main/java/com/quickboard/apicomposer/post/controller/TestController.java
@@ -1,37 +1,15 @@
 package com.quickboard.apicomposer.post.controller;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-import java.time.Duration;
 
 @RestController
 @RequestMapping("/composition/v1")
 public class TestController {
 
-    private final WebClient webClient;
-
-    public TestController(@Qualifier("postWebClient")WebClient webClient) {
-        this.webClient = webClient;
-    }
-
     @GetMapping("/tests/hello-world")
     public String helloWorld(){
         return "hello world";
-    }
-
-    @GetMapping("/boards/{id}/posts")
-    public Mono<String> getPosts(@PathVariable("id") Long boardId){
-        return webClient.get().uri("/rsc/v1/boards/" + boardId + "/posts")
-                .retrieve()
-                .onStatus(code -> code.is4xxClientError(),
-                        resp -> resp.bodyToMono(String.class).map(RuntimeException::new))
-                .bodyToMono(String.class)
-                .timeout(Duration.ofSeconds(2));
     }
 }
